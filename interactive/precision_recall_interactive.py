d8e67149ff75de02f0b2e4ffedf3115ca2cf4330
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-04-08 10:40:52 -0500

    update precision recall interactive

--- interactive/precision_recall_interactive.py
+++ interactive/precision_recall_interactive.py
@@ -8,23 +8,8 @@ def precision_recall_viz(treshold=0):
   np.random.seed(10)
   positive_class = pd.DataFrame(dict(x=np.linspace(-3, 10, 10), y=np.ones(10, dtype=int)))
   negative_class = pd.DataFrame(dict(x=np.linspace(-10, 3, 10), y=np.zeros(10, dtype=int)))
-  test_data = pd.concat([positive_class, negative_class])
-
-  # precision and recall
-  tp = len(positive_class[positive_class.x >= treshold])
-  tn = len(negative_class[negative_class.x < treshold])
-  fp = len(negative_class[negative_class.x > treshold])
-  fn = len(positive_class[positive_class.x <= treshold])
-
-  precision = tp / (tp + fp)
-  recall = tp / (tp + fn)
 
   plt.figure(figsize=(12, 7))
-  plt.text(3, -1.5, f"TP: {tp} FP: {fp}", size=15)
-  plt.text(-8, -1.5, f"TN: {tn} FN: {fn}", size=15)
-  plt.title(f"Precision: {precision:.2f} | Recall: {recall:.2f}", size=17)
-
-  # data scatterplots
   sns.scatterplot(
       x=positive_class.x, 
       y=0.2 * np.ones(len(positive_class.x)), 
@@ -36,6 +21,19 @@ def precision_recall_viz(treshold=0):
       label="Negative class"
       )
 
+  # precision and recall
+  tp = len(positive_class[positive_class.x >= treshold])
+  tn = len(negative_class[negative_class.x < treshold])
+  fp = len(negative_class[negative_class.x > treshold])
+  fn = len(positive_class[positive_class.x <= treshold])
+
+  precision = tp / (tp + fp)
+  recall = tp / (tp + fn)
+
+  plt.text(3, -1.5, f"TP: {tp} FP: {fp}", size=15)
+  plt.text(-8, -1.5, f"TN: {tn} FN: {fn}", size=15)
+  plt.title(f"Precision: {precision:.2f} | Recall: {recall:.2f}", size=17)
+  
   # tresholds and decision regions
   plt.vlines(treshold, -2, 3, label="Treshold")
   plt.axvspan(treshold, 11, alpha=0.2)
