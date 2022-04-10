ed3d9be430d072ce6cbf84cb2d9fac6d1b89ccaa
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-04-10 17:08:46 -0500

    add decision regions

--- interactive/svm_soft_margin_interactive.py
+++ interactive/svm_soft_margin_interactive.py
@@ -7,8 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 
+from interactive_utils import (
+    get_grid, 
+    plot_decision_regions,
+    plot_margins
+    )
 
-def svm_soft_margin_viz(C):
+def interactive_svm_viz(C):
   # data scatterplot
   penguins = sns.load_dataset("penguins")
   penguins = penguins.loc[penguins.species != "Gentoo"].dropna()
@@ -28,18 +33,9 @@ def svm_soft_margin_viz(C):
   # decision regions and margin
   xlim = ax.get_xlim()
   ylim = ax.get_ylim()
-  x1_grid = np.linspace(xlim[0], xlim[1])
-  x2_grid = np.linspace(ylim[0], ylim[1])
-
-  xx, yy = np.meshgrid(x1_grid, x2_grid)
-  r1, r2 = np.c_[xx.flatten()], np.c_[yy.flatten()]
-  grid = pd.DataFrame(np.hstack((r1, r2)), columns=X.columns)
-
-  z_decision_function = svm_clf.decision_function(grid).reshape(xx.shape)
-  z_predict = svm_clf.predict(grid).reshape(xx.shape)
-
-  ax.contour(xx, yy, z_decision_function, colors="k", levels=[-1, 0, 1], linestyles=["--", "-", "--"], alpha=0.5)
-  ax.contourf(xx, yy, z_predict, cmap='Paired', alpha=0.2)
+  xx, yy, grid = get_grid(xlim, ylim, X.columns)
+  plot_margins(svm_clf, xx, yy, grid, ax)
+  plot_decision_regions(svm_clf, xx, yy, grid, ax)
 
   # support vectors
   support_vectors = scaler.inverse_transform(svm_clf["svc"].support_vectors_)
