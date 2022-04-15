cd9cc17bedb5176b121bb96a0bc861110dcb62a3
daniel ocampo <daniel.ocampoh@udea.edu.co>
2022-04-14 19:56:02 -0500

    update

--- interactive/pca_kernel_interactive.py
+++ interactive/pca_kernel_interactive.py
@@ -1,31 +1,27 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-import plotly.express as px
-
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_breast_cancer
 from sklearn.decomposition import KernelPCA
 
+import plotly.express as px
 
-def pca_kernel_viz(standarized=True, n_components=2, kernel="linear", gamma=None, degree=2, coef0=1):
+
+def pca_kernel_viz(kernel="linear", coef0=1, n_components=2,
+                   standarized=True, gamma=None, degree=2):
+
   data = load_breast_cancer()
   X = data.data
   y = data.target
 
-  pca = KernelPCA(n_components=n_components, 
-                  kernel=kernel, 
-                  gamma=gamma, 
-                  degree=degree, 
-                  coef0=coef0)
+  pca = KernelPCA(n_components=n_components, kernel=kernel, 
+                  gamma=gamma, degree=degree, coef0=coef0)
 
   X_pca = make_pipeline(StandardScaler(), pca).fit_transform(X) if standarized else pca.fit_transform(X)
 
+  labels = {"x": "PC1", "y": "PC2"} if n_components == 2 else {"x": "PC1", "y": "PC2", "z": "PC3"}
+
   if n_components == 2:
-    fig = px.scatter(x=X_pca[:,0], y=X_pca[:,1], color=y, labels={"x": "PC1", "y": "PC2"})
-    fig.show()
+    fig = px.scatter(x=X_pca[:,0], y=X_pca[:,1], color=y, labels=labels)
   elif n_components == 3:
-    fig = px.scatter_3d(x=X_pca[:,0], y=X_pca[:,1], z=X_pca[:,2], color=y, labels={"x": "PC1", "y": "PC2", "z": "PC3"})
-    fig.show()
+    fig = px.scatter_3d(x=X_pca[:,0], y=X_pca[:,1], z=X_pca[:,2], color=y, labels=labels)
+  fig.show()
